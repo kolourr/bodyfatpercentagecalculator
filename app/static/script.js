7fd22bfdfbbe4e2064f41e0841f38cf0a4a23592
brebllo <bruce.rebello@gmail.com>
2019-06-24 07:20:39 -0400

    Changed gif alignment and created new buttons

--- app/static/script.js
+++ app/static/script.js
@@ -1,17 +1,17 @@
-// ( function( $ ) {
-// $( document ).ready(function() {
-// $('#cssmenu').prepend('<div id="menu-button">Menu</div>');
-// 	$('#cssmenu #menu-button').on('click', function(){
-// 		var menu = $(this).next('ul');
-// 		if (menu.hasClass('open')) {
-// 			menu.removeClass('open');
-// 		}
-// 		else {
-// 			menu.addClass('open');
-// 		}
-// 	});
-// });
-// } )( jQuery );
+( function( $ ) {
+$( document ).ready(function() {
+$('#cssmenu').prepend('<div id="menu-button">Menu</div>');
+	$('#cssmenu #menu-button').on('click', function(){
+		var menu = $(this).next('ul');
+		if (menu.hasClass('open')) {
+			menu.removeClass('open');
+		}
+		else {
+			menu.addClass('open');
+		}
+	});
+});
+} )( jQuery );
 
 
 
@@ -351,20 +351,7 @@ function dataURItoBlob(dataURI) {
 
 
 
-( function( $ ) {
-$( document ).ready(function() {
-$('#cssmenu').prepend('<div id="menu-button">Menu</div>');
-	$('#cssmenu #menu-button').on('click', function(){
-		var menu = $(this).next('ul');
-		if (menu.hasClass('open')) {
-			menu.removeClass('open');
-		}
-		else {
-			menu.addClass('open');
-		}
-	});
-});
-} )( jQuery );
+
 
 
 
