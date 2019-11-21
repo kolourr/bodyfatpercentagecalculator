ada98aca804e7ea1cf5b7d9afb9da6790056469d
Bruce <50345963+estimatebodyfat@users.noreply.github.com>
2019-11-21 14:07:59 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,6 +1,6 @@
 """
-This application was built on based on Jeremy Howard's Bear Classifier Webapp. 
-It uses the Fast AI Library, Intel's Haarcascades and Croppie JS plugin
+This application was built based on Jeremy Howard's Bear Classifier Webapp. It uses the Fast AI Library, Intel's Haarcascades and Croppie JS plugin
+
 """
 
 
