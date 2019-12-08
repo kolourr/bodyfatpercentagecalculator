9a4cdc3370c0279bfbd9097a722045e69b3770f9
brebllo <bruce.rebello@gmail.com>
2019-12-08 17:58:23 -0500

    new redirect response

--- app/server.py
+++ app/server.py
@@ -16,7 +16,7 @@ from fastai.vision import *
 from io import BytesIO
 from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
-from starlette.responses import HTMLResponse, JSONResponse
+from starlette.responses import HTMLResponse, JSONResponse, PlainTextResponse, RedirectResponse
 from starlette.staticfiles import StaticFiles
 from PIL import Image
 
@@ -236,10 +236,19 @@ async def sitemap(request):
     return HTMLResponse(html_file.open().read())
 
 
-@app.route('/how-to-lose-weight-fast-and-easy.html')
-async def sitemap(request):
-    html_file = path / 'view' / 'how-to-lose-weight-fast-without-exercise.html'
-    return HTMLResponse(html_file.open().read())
+# @app.route('/how-to-lose-weight-fast-and-easy.html')
+# async def sitemap(request):
+#     html_file = path / 'view' / 'how-to-lose-weight-fast-without-exercise.html'
+#     return HTMLResponse(html_file.open().read())
+
+
+async def app(scope, receive, send):
+    assert scope['type'] == 'http'
+    if scope['path'] != '/how-to-lose-weight-fast-and-easy.html':
+        response = RedirectResponse(url='/how-to-lose-weight-fast-without-exercise.html')
+    else:
+        response = PlainTextResponse('Hello, world!')
+    await response(scope, receive, send)
 
 
 @app.route('/analyze', methods=['POST'])
