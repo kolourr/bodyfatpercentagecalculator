3406075f3908060ac9350b04d34c93e954d664a4
brebllo <bruce.rebello@gmail.com>
2020-06-17 22:21:27 -0400

    added new price

--- app/server.py
+++ app/server.py
@@ -341,7 +341,7 @@ async def initiatepayment(request):
     publishable_key = os.getenv("stripe_publishable_key")
 
     # amount in cents
-    amount_to_pay = 2499
+    amount_to_pay = 499
 
     intent = stripe.PaymentIntent.create(
         amount=amount_to_pay,
