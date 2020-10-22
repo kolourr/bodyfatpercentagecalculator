20642e865142f85b37c740f5ddafa7ae0bce3b32
brebllo <bruce.rebello@gmail.com>
2020-10-22 14:17:30 -0400

    new copy and price

--- app/static/stripe.js
+++ app/static/stripe.js
@@ -59,7 +59,7 @@ function start_payment(paymentData, paymentCallback) {
             currency: 'usd',
             total: {
                 label: '3 Body Fat % Estimates',
-                amount: 500,
+                amount: 499,
             },
             requestPayerName: true,
             requestPayerEmail: true,
