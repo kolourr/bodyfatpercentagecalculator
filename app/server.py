c435e40e6c54832e5d36e69d7012f63f08cb40b1
brebllo <bruce.rebello@gmail.com>
2020-11-05 18:22:52 -0500

    added keto series

--- app/server.py
+++ app/server.py
@@ -354,6 +354,11 @@ async def initiatepayment(request):
         """
         Thanks for purchasing the 3 Body Fat Estimations.
 
+        For your Free gift, here's the Google Drive download link for the Keto ebook, audio and video series:
+
+        https://drive.google.com/uc?export=download&id=1uxifFRI8hbJFQ36fIUgDQOQugRVgs38I
+
+
         """,
         # Verify your integration in this guide by including this parameter
         metadata={'integration_check': 'accept_a_payment'},
