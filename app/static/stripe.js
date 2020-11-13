3db5f1ea0ca8ff5ec9710e9daba68464968e9c03
brebllo <bruce.rebello@gmail.com>
2020-11-13 11:24:23 -0500

    copy

--- app/static/stripe.js
+++ app/static/stripe.js
@@ -173,7 +173,7 @@ function init(content) {
         </div>
       </div>
       <div class="ElementsModal--price ElementsModal--details">${amount}</div>
-      <div style="color: white;font-size: 15px; font-weight: 550; margin-top: 10px;">That's only $1.66/estimate. Body Fat % displayed immediately once payment made.</div>
+      <div style="color: white;font-size: 15px; font-weight: 550; margin-top: 10px;">That's only $1.66 per estimate.</div>
       <div style="color: rgba(255, 255, 255, 0.6); font-size: 14px; font-weight: 500; margin-top: 10px;">100% Money Back Guarantee. If unsatisfied, email contact@estimatebodyfat.com for full refund.</div>
 
 
