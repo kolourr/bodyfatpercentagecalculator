a46782abd75a4a76a7e225243451339b39ce4961
estimatebodyfat <contact@estimatebodyfat.com>
2021-01-30 19:16:18 -0500

    payment

--- app/static/stripe.js
+++ app/static/stripe.js
@@ -168,7 +168,8 @@ function init(content) {
             <div style="color: #E28C57;font-size: 23px; font-weight: 550; margin-top: 10px;">$9.99 for 3 Body Fat % estimates</div>
         </div>
       </div>
-      <div style="color:#FBC7A2;font-size: 15px; font-weight: 550; margin-top: 10px;">Body Fat % will be displayed immediately after payment. If you have any concerns at all, remember, you can try us risk free. Email contact@estimatebodyfat.com for full refund if unsatisfied.</div>
+      <div style="color:#F2FAFA;font-size: 15px; font-weight: 550; margin-top: 10px;">Body Fat % will be displayed immediately below.</div>
+      <div style="color:#F2FAFA;font-size: 15px; font-weight: 550; margin-top: 10px;">You can try us 100% risk free. Email contact@estimatebodyfat.com for full refund if unsatisfied.</div>
       <!-- <div class="ElementsModal--price ElementsModal--details">${amount}</div> -->
     </div>
   </div>
